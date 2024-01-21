c28d81be92da83658e5ae79b44184145fed905cb
Nikolaev Andrey <nikolaev_34@mail.ru>
2024-01-21 20:47:36 +0300

    Initial commit

---
+++ .venv/data.py
@@ -0,0 +1,11 @@
+order_body = {
+    "firstName": "Иван",
+    "lastName": "Сидоров",
+    "address": "г.Москва,ул.Ленина,35,325",
+    "metroStation": 1,
+    "phone": "+79038003535",
+    "rentTime": 3,
+    "deliveryDate": "2024-01-28",
+    "comment": "Пусть всегда будет солнце!",
+    "color": ["BLACK"]
+}
---
+++ configuration.py
@@ -0,0 +1,4 @@
+URL_SERVICE = "https://48034f66-65cd-4aff-9be0-52f6530ac259.serverhub.praktikum-services.ru/"  # адрес сервера
+CREATE_COURIER = "/api/v1/courier"   # api для создание курьера
+CREATE_ORDER = "/api/v1/orders"  # api для создание заказа
+ORDER_INFORMATION = "/api/v1/orders/track?t="   # api для получения информации о заказе по его номеру
